6ea82d122e48117a67229108fc31ac5c03a11ba7
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2023-10-26 10:23:30 +0800

    Add Trainer._train_show()

--- common/trainer.py
+++ common/trainer.py
@@ -32,30 +32,34 @@ class Trainer:
                 y = self.model.forward(x_batch)
 
                 total_loss += self.model.loss(y, t_batch)
-
                 train_accu_count += np.sum(y.argmax(axis=1) == t_batch)
 
                 self.model.backward()
+
                 self.optimizer.update()
                 self.optimizer.zero_grad()
 
                 if self.show_per_iter and ((iter + 1) % self.show_per_iter == 0 or iter + 1 == goal_iter):
-                    interval_iter = self.show_per_iter if (iter + 1) % self.show_per_iter == 0 \
-                        else goal_iter % self.show_per_iter
-
-                    average_loss = total_loss / interval_iter
-                    self.loss_list.append(float(average_loss))
-
-                    train_accuracy = train_accu_count / (batch_size * interval_iter)
-
-                    message = f'| epoch {epoch + 1:{len(str(goal_epoch))}} ' \
-                              f'| iter {iter + 1:{len(str(goal_iter))}}/{goal_iter} ' \
-                              f'| loss {average_loss:.4f} ' \
-                              f'| train accuracy {train_accuracy:.4f}s' \
-                              f'| time {time.time() - start_time:.2f}s'
-
-                    progress_bar(iter, goal_iter, message=message, break_line=(iter + 1 == goal_iter))
+                    self._train_show(epoch, iter, goal_epoch, goal_iter, batch_size,
+                                     total_loss, train_accu_count, start_time)
                     total_loss, train_accu_count = 0, 0
+
+    def _train_show(self, epoch, iter, goal_epoch, goal_iter, batch_size, total_loss, train_accu_count, start_time):
+        interval_iter = self.show_per_iter if (iter + 1) % self.show_per_iter == 0 \
+            else goal_iter % self.show_per_iter
+
+        average_loss = total_loss / interval_iter
+        self.loss_list.append(float(average_loss))
+
+        train_accuracy = train_accu_count / (batch_size * interval_iter)
+
+        message = f'| epoch {epoch + 1:{len(str(goal_epoch))}} ' \
+                  f'| iter {iter + 1:{len(str(goal_iter))}}/{goal_iter} ' \
+                  f'| loss {average_loss:.4f} ' \
+                  f'| train accuracy {train_accuracy:.4f}s ' \
+                  f'| time {time.time() - start_time:.2f}s '
+
+        progress_bar(iter, goal_iter, message=message, break_line=(iter + 1 == goal_iter))
 
     def plot(self):
         x = numpy.arange(len(self.loss_list))
